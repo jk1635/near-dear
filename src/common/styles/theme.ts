bbda67770fce4bd7b2d8bb478cbb02e5068001c1
jk <jkwak1635@gmail.com>
2024-03-24 02:15:08 +0900

    feat: 글로벌 스타일 및 테마 수정

--- src/common/styles/theme.ts
+++ src/common/styles/theme.ts
@@ -5,18 +5,18 @@ const theme: Theme = {
         primary: '#fef3d3',
         black: '#141618',
         white: '#ffffff',
-        gray: '#f2f5f7',
+        gray: '#c2cfd9',
     },
     typography: {
-        t1: { fontSize: '1.5rem', fontWeight: 700, lineHeight: '1.5' },
-        t2: { fontSize: '1.25rem', fontWeight: 500, lineHeight: '1.4' },
+        t1: { fontSize: '1.5rem', fontWeight: 500, lineHeight: '1.5' },
+        t2: { fontSize: '1.25rem', fontWeight: 700, lineHeight: '1.4' },
         t3: { fontSize: '1.125rem', fontWeight: 700, lineHeight: '1.5' },
-        t4: { fontSize: '1.125rem', fontWeight: 600, lineHeight: '1.5' },
+        t4: { fontSize: '1.125rem', fontWeight: 400, lineHeight: '1.5' },
         t5: { fontSize: '1rem', fontWeight: 500, lineHeight: '1.5' },
         t6: { fontSize: '0.875rem', fontWeight: 400, lineHeight: '1.4' },
         t7: { fontSize: '0.75rem', fontWeight: 400, lineHeight: '1.4' },
         caption: { fontSize: '0.75rem', fontWeight: 700, lineHeight: '1.4' },
-        button: { fontSize: '1rem', fontWeight: 700, lineHeight: '1.5' },
+        button: { fontSize: '0.875rem', fontWeight: 700, lineHeight: '1.5' },
     },
 };
 
