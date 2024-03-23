c9a4fed40a9e54bf280acdc5b1e68bdea0bf87ba
jk <jkwak1635@gmail.com>
2024-03-24 02:16:21 +0900

    feat: Button에 variant 스타일, fullWidth 옵션 추가

--- src/common/components/Button.tsx
+++ src/common/components/Button.tsx
@@ -1,38 +1,73 @@
 import { forwardRef, ReactNode, ButtonHTMLAttributes } from 'react';
 
+import { Theme } from '@emotion/react';
 import styled from '@emotion/styled';
+
+type ButtonVariant = 'basic' | 'gray' | 'outline';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     children: ReactNode;
+    variant?: ButtonVariant;
     fullWidth?: boolean;
 }
 
-const Button = forwardRef<HTMLButtonElement, ButtonProps>(({ children, ...rest }, ref) => {
-    return (
-        <ButtonWrapper ref={ref} {...rest}>
-            <Label>{children}</Label>
-        </ButtonWrapper>
-    );
-});
+type VariantStyles = {
+    variant?: ButtonVariant;
+    theme: Theme;
+};
 
-const ButtonWrapper = styled.button`
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+    ({ fullWidth = true, variant = 'basic', children, ...rest }, ref) => {
+        return (
+            <ButtonWrapper fullWidth={fullWidth} ref={ref} variant={variant} {...rest}>
+                <span>{children}</span>
+            </ButtonWrapper>
+        );
+    }
+);
+
+const variantStyles = ({ variant, theme }: VariantStyles) => {
+    switch (variant) {
+        case 'outline':
+            return {
+                marginBottom: '1rem',
+                padding: '1.5rem',
+                border: `1px solid ${theme.colors.gray}`,
+                backgroundColor: theme.colors.white,
+                ...theme.typography.t6,
+                '&:last-of-type': {
+                    marginBottom: '0',
+                },
+                '&:hover': {
+                    backgroundColor: theme.colors.gray,
+                },
+            };
+        case 'gray':
+            return {
+                backgroundColor: 'transparent',
+                color: theme.colors.gray,
+                ...theme.typography.caption,
+            };
+        default:
+            return {
+                backgroundColor: theme.colors.primary,
+                color: theme.colors.black,
+                height: '3.5rem',
+                ...theme.typography.button,
+            };
+    }
+};
+
+const ButtonWrapper = styled.button<ButtonProps>`
     display: flex;
     justify-content: center;
     align-items: center;
+    width: ${({ fullWidth }) => (fullWidth ? '100%' : 'auto')};
     border: 0;
-    width: 100%;
-    height: 3.5rem;
-    padding: 0 1.5rem;
     border-radius: 0.75rem;
-    background-color: ${({ theme }) => theme.colors.primary};
     cursor: pointer;
-`;
-
-const Label = styled.span`
-    color: ${({ theme }) => theme.colors.black};
-    font-size: 0.875rem;
-    font-weight: bold;
     text-decoration: none;
+    ${variantStyles};
 `;
 
 export default Button;
