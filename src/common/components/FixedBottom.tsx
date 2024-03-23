2b98f8d6d34f71b9266b4dfaa9097e46331660fe
jk <jkwak1635@gmail.com>
2024-03-24 02:15:52 +0900

    feat: FixedBottom 버튼 여러개 적용 가능하도록 수정

--- src/common/components/FixedBottom.tsx
+++ src/common/components/FixedBottom.tsx
@@ -1,4 +1,4 @@
-import { ComponentPropsWithoutRef, forwardRef } from 'react';
+import { Children, ComponentPropsWithoutRef, forwardRef, ReactNode } from 'react';
 
 import styled from '@emotion/styled';
 
@@ -6,15 +6,19 @@ import { GlobalPortal } from '@/GlobalPortal';
 
 import Button from './Button';
 
-type FixedBottomProps = ComponentPropsWithoutRef<typeof Button>;
+type FixedBottomProps = ComponentPropsWithoutRef<typeof Button> & {
+    children: ReactNode;
+};
 
-const FixedBottom = forwardRef<HTMLButtonElement, FixedBottomProps>(({ children, ...rest }, ref) => {
+const FixedBottom = forwardRef<HTMLDivElement, FixedBottomProps>(({ children }, ref) => {
     return (
         <GlobalPortal.Consumer>
             <BottomContainer>
-                <Button {...rest} ref={ref}>
-                    {children}
-                </Button>
+                {Children.map(children, (child, index) => (
+                    <ButtonWrapper key={index} ref={ref}>
+                        {child}
+                    </ButtonWrapper>
+                ))}
             </BottomContainer>
         </GlobalPortal.Consumer>
     );
@@ -31,6 +35,16 @@ const BottomContainer = styled.div`
     padding: 1rem 1.5rem;
     background: linear-gradient(rgba(255, 255, 255, 0) 0%, rgb(255, 255, 255) 16px);
     transform: translateX(-50%);
+`;
+
+const ButtonWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    margin-bottom: 1rem;
+
+    &:last-of-type {
+        margin-bottom: 0;
+    }
 `;
 
 export default FixedBottom;
