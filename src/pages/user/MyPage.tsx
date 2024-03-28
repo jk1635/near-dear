a156660e0c9620fe5baf90f21fb749f8d6c4edcc
lea <leaisrevolution@opusm.io>
2024-03-28 20:39:23 +0900

    Add: List 컴포넌트

--- src/pages/user/MyPage.tsx
+++ src/pages/user/MyPage.tsx
@@ -3,12 +3,13 @@ import { useRecoilState } from 'recoil';
 import Container from '@common/components/Container.tsx';
 
 import { userTypeState } from '@/common/stores/atoms';
+import UserCard from './UserCard';
 
 const MyPage = () => {
-    const userTaye = useRecoilState(userTypeState);
+    // const userTaye = useRecoilState(userTypeState);
     return (
         <Container>
-            <h1>MyPage</h1>
+            <UserCard />
         </Container>
     );
 };
--- src/pages/user/UserCard.tsx
+++ src/pages/user/UserCard.tsx
@@ -3,7 +3,24 @@ import React from 'react';
 import styled from '@emotion/styled';
 
 const UserCard = () => {
-    return <div></div>;
+    return (
+        <UserCardContainer>
+            <ProfileImg></ProfileImg>
+            <Information>
+                <Name></Name>
+                <Email></Email>
+            </Information>
+        </UserCardContainer>
+    );
 };
+
+const UserCardContainer = styled.div``;
+const ProfileImg = styled.img`
+    border-radius: 50%;
+    object-fit: cover;
+`;
+const Information = styled.div``;
+const Name = styled.div``;
+const Email = styled.div``;
 
 export default UserCard;
