0001382732c15855e4a6aa59ecf6dee6e2420924
jk <jkwak1635@gmail.com>
2024-04-10 17:14:26 +0900

    feat: baseUrl 추가

--- src/common/utils/apiClient.ts
+++ src/common/utils/apiClient.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError, AxiosResponse } from 'axios';
 
-const baseURL = '';
+const baseURL = import.meta.env.VITE_API_BASE_URL;
 
 const axiosInstance = axios.create({
     baseURL,
