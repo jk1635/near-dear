53e972c49543e2f6cfe0d33ca1d32d47cf3128ae
jk <jkwak1635@gmail.com>
2024-04-10 17:14:41 +0900

    feat: react-query 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 
+import { QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 
 import App from '@/App.tsx';
 
+const queryClient = new QueryClient({
+    queryCache: new QueryCache({
+        onError: error => {
+            console.error(`문제가 발생했습니다. ${error.message}`);
+        },
+    }),
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <RecoilRoot>
-            <App />
-        </RecoilRoot>
+        <QueryClientProvider client={queryClient}>
+            <RecoilRoot>
+                <App />
+            </RecoilRoot>
+        </QueryClientProvider>
     </React.StrictMode>
 );
