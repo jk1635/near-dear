7c2e2a327c22fc2a9f74ba39ad59bba3fea20a73
jk <jkwak1635@gmail.com>
2024-04-12 23:19:43 +0900

    feat: Text typography에 맞게 업데이트

--- src/common/components/Text.tsx
+++ src/common/components/Text.tsx
@@ -2,18 +2,18 @@ import { ReactNode } from 'react';
 
 import styled from '@emotion/styled';
 
-type TitleVariant = 't1' | 't2' | 't3' | 't4';
+type TitleVariant = 'title1' | 'title2' | 'large_text' | 'normal_text' | 'small_text' | 'button';
 
 interface TitleProps {
     children: ReactNode;
     variant?: TitleVariant;
 }
 
-const Text = ({ children, variant = 't2' }: TitleProps) => {
+const Text = ({ children, variant = 'title2' }: TitleProps) => {
     return <MainText variant={variant}>{children}</MainText>;
 };
 
-const MainText = styled.h2<{ variant: 't1' | 't2' | 't3' | 't4' }>`
+const MainText = styled.h2<{ variant: 'title1' | 'title2' | 'large_text' | 'normal_text' | 'small_text' | 'button' }>`
     ${({ theme, variant }) => ({ ...theme.typography[variant] })};
     text-align: center;
 `;
