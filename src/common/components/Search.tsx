d4294b33c682c884d989fe5537c838b6ba118daf
lea <leaisrevolution@opusm.io>
2024-04-20 14:29:41 +0900

    Edit: placeholder 추가 및 디자인 변경

--- src/common/components/Search.tsx
+++ src/common/components/Search.tsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import styled from '@emotion/styled';
 
 const Search = () => {
-    return <div></div>;
+    const [search, setSearch] = useState('');
+    console.log('search', search);
+    return (
+        <Container>
+            <SearchDiv>
+                <Input onChange={e => setSearch(e.target.value)} placeholder="지역, 상품, 매장명 검색" />
+            </SearchDiv>
+        </Container>
+    );
 };
+
+const Container = styled.div`
+    width: 90%;
+    height: 100%;
+    padding: 10px 20px;
+`;
+
+const SearchDiv = styled.div`
+    width: 100%;
+    height: 100%;
+    background-color: ${({ theme }) => theme.colors.primary};
+    border-radius: 15px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 10px 20px;
+`;
+
+const Input = styled.input`
+    width: 100%;
+    border: none;
+    background-color: transparent;
+`;
 
 export default Search;
