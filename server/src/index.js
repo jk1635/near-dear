e172ff104501e0d9eba1a6a737bc48acc093428b
jk <jkwak1635@gmail.com>
2024-04-24 00:05:18 +0900

    feat: MongoDB 연결 및 에러 처리기 추가

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,49 @@
 const express = require('express');
 
-const app = express();
-const port = 4000;
+const path = require('path');
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+const app = express();
+const cors = require('cors');
+
+const port = 4000;
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB 연결 완료');
+  })
+  .catch(err => {
+    console.error(err);
+  });
+
+app.get('/', (req, res,next) => {
+  setImmediate(() => {
+    next(new Error('It is an error'));
+  });
+  // res.send('Hello World');
 });
 
-app.listen(4000, () => {
-    console.log(`${port}번에서 실행되었습니다.`);
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.json(req.body);
+});
+
+// app.use('/users', require('./routes/users'));
+
+app.use((error, req, res, next) => {
+  res.status(err.status || 500);
+  res.send(error.message || 'Internal Server Error');
+})
+
+app.use(express.static(path.join(__dirname, '../uploads')));
+
+app.listen(port, () => {
+  console.log(`${port}번에서 실행되었습니다.`);
 });
